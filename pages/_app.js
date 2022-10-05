fd91eb05319fa37617908bb3f52cf2cb32681d39
Viviane Dias <vivianedias@protonmail.com>
2022-10-05 10:30:42 -0300

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import theme from "../shared/theme";
+
+const extendedTheme = extendTheme(theme);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={extendedTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp
