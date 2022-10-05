4beaab33e5217e2bb7996bfa063c86a09a801563
Viviane Dias <vivianedias@protonmail.com>
2022-10-05 12:07:55 -0300

    feat: adds i18n setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,29 @@
 import '../styles/globals.css'
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { appWithTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import { Header, Footer } from "../shared/components";
 import theme from "../shared/theme";
 
 const extendedTheme = extendTheme(theme);
 
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common", "header", "footer"])),
+    },
+  };
+}
+
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={extendedTheme}>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </ChakraProvider>
   );
 }
 
-export default MyApp
+export default appWithTranslation(MyApp);
